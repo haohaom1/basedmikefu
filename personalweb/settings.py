19efcf9a391ce713c45f5c02751c79a020470f4e
Mike Fu <xuanhaofu@gmail.com>
2019-01-04 17:45:25 -0500

    remove keys

--- personalweb/settings.py
+++ personalweb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1ocm7k0ry6%k%+!kb^ut4hbe)xm_a9i&&1x5rt#7ao(wr4bs@('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
